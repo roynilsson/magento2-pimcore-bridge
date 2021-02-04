77041880d607c253ff06dc95c1246b56219ac0c0
Roy Nilsson <roy@caupo.se>
2021-02-04 16:43:19 +0100

    Handle duplicate categories in data from Pim. Caused by old bug in our pim implementation

--- Queue/Action/Product/CategoryIdsModifier.php
+++ Queue/Action/Product/CategoryIdsModifier.php
@@ -79,6 +79,8 @@ class CategoryIdsModifier implements DataModifierInterface
     {
         $pimCatIds = $pimcoreProduct->getData('category_ids') ?? [];
 
+        $pimCatIds = array_unique($pimCatIds);
+
         $catCollection = $this->getMageCatCollection($pimcoreProduct);
         $mageCatIds = $catCollection->getAllIds();
         $pimcoreProduct->setData('category_ids', $mageCatIds);
